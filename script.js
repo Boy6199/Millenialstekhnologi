dfd17884f072e63482fb95a57774f1de080b533c
Boy6199 <150954169+Boy6199@users.noreply.github.com>
2025-03-19 13:59:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ const arrGame = [
     img: "https://millenialstekhnologi.com/logo/arena1.webp",
   },
   {
-    href: "https://waroengppob.pusatdigi.com/digital/328343",
+    href: "https://millenialstekhnologi.com/ajax2.0/higgs.html",
     identity: null,
     name: "Higgs Games",
     hot: "HOT",
